3d0af533d702a1ac2cd64fdbf1b4a7adb1a984ba
nexus <emekaezeobi@outlook.com>
2023-07-13 14:30:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C programe that prints without using the printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+
+'return :0;'
+}
