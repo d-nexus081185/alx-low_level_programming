8228645c093bf9b324bc225e0d121f424ceb816a
nexus <emekaezeobi@outlook.com>
2023-07-14 06:10:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the Alphabets.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; < 26; i++)
+{
+putchar("alp[i]);
+}
+putchar('\n');
+return (0);
+}
