f5c6d5fc273f67f0df3d3a9a336b31cf44b639d5
nexus <emekaezeobi@outlook.com>
2023-07-26 16:11:19 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,10 +3,9 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- *
  * Return: void (success)
  */
-char *_strcat(char*dest, char*src)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 	i = 0;
