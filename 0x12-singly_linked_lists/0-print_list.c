6854cf893b911a6601666d9bc03f3163ef50bf53
Chukwuemeka Ezeobi <emekaezeobi@outlook.com>
2023-08-25 00:40:58 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
+ * print_list - A program that prints all the elements of a linked list 
  * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes printed
+ * Return: (s) (success)
  */
 size_t print_list(const list_t *h)
 {
