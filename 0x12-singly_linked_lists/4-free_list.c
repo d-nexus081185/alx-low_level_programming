14b0bec67cc6335e9ad6b340b16f61cb505cacc4
Chukwuemeka Ezeobi <emekaezeobi@outlook.com>
2023-08-25 01:18:22 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,14 +2,14 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
+ * free_list - A programe that frees a linked list
  * @head: list_t list to be freed
  */
 void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head)
+	for (head)
 	{
 		temp = head->next;
 		free(head->str);
