38cb4e7ce9ae6ed4e254ac91cb0c1a4273bac170
Chukwuemeka Ezeobi <emekaezeobi@outlook.com>
2023-08-25 00:46:49 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,11 +3,11 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a linked list
+ * Description - A program that adds a new node at the end of a linked list
  * @head: double pointer to the list_t list
  * @str: string to put in the new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: (new) to show end of code
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
