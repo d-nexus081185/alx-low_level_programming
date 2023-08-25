2528c966aa4ce368c5148a74b3bcb2bbffcb68a4
Chukwuemeka Ezeobi <emekaezeobi@outlook.com>
2023-08-25 00:43:09 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
+ * list_len - A program that returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
- * Return: number of elements in h
+ * Return: (n) to show success
  */
 size_t list_len(const list_t *h)
 {
